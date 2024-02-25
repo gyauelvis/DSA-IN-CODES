0b360c3ec483a821f98f9eb2b9200f1ad1d186d4
gyauelvis <gyauelvis@gmail.com>
2024-02-25 03:30:49 +0000

    duplicate question

---
+++ LeetCodeProblems/duplicateII.cpp
@@ -0,0 +1,44 @@
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+using namespace std;
+bool containsNearbyDuplicate(vector<int> &nums, int k)
+{
+    unordered_map<int, int> map;
+    map[nums[0]] = 0;
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (map.find(nums[i]) != map.end() && abs(i - nums[i]) <= k)
+            return true;
+        else
+            map[nums[i]] = i;
+    }
+    return false;
+}
+
+int main()
+{
+    vector<int> nums = {1,2,3,1,2,3};
+    int k = 3;
+    cout << containsNearbyDuplicate(nums, k) << endl;
+    return 0;
+}
